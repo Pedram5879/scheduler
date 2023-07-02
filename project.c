41d25c1b7497567b1c90074d48a90c69746462aa
Pedram Vahdati Rohani <82025482+Pedram5879@users.noreply.github.com>
2023-07-03 00:50:47 +0330

    add Task structure

---
+++ project.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    char task_type;
+    int execution_time;
+    char state[10];
+    int time_on_processor;
+} Task;
+
+int main() {
+    return 0;
+}
