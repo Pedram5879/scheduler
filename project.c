f5eb3353631b76d4a06305272c08ab33e60b3bed
Pedram Vahdati Rohani <82025482+Pedram5879@users.noreply.github.com>
2023-07-03 01:39:14 +0330

    add allocate_resources function

--- project.c
+++ project.c
@@ -8,6 +8,28 @@ typedef struct {
     int time_on_processor;
 } Task;
 
+int resources_R1, resources_R2, resources_R3;
+
+Task ready_queue[100];
+int ready_queue_size = 0;
+Task waiting_queue[100];
+int waiting_queue_size = 0;
+
+void allocate_resources(Task task) {
+    if (task.task_type == 'X') {
+        resources_R1 -= 1;
+        resources_R2 -= 1;
+    }
+    else if (task.task_type == 'Y') {
+        resources_R2 -= 1;
+        resources_R3 -= 1;
+    }
+    else if (task.task_type == 'Z') {
+        resources_R1 -= 1;
+        resources_R3 -= 1;
+    }
+}
+
 int main() {
     return 0;
 }
