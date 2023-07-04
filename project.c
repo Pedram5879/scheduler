4ecdaed4bec682260640c7e8c1c6d2609a1b7ee5
Pedram Vahdati Rohani <82025482+Pedram5879@users.noreply.github.com>
2023-07-04 04:29:45 +0330

    added allocate function

--- project.c
+++ project.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 typedef struct {
+    char task_name[10];
     char task_type;
     int execution_time;
     char state[10];
@@ -10,11 +12,23 @@ typedef struct {
 
 int resources_R1, resources_R2, resources_R3;
 
-Task ready_queue[100];
+Task ready_queue[100];      // Ready queue for tasks ready to be executed (priority queue)
+Task priority_queue[100];   // priority queue
+int priority_queue_size = 0; 
 int ready_queue_size = 0;
-Task waiting_queue[100];
+Task waiting_queue[100];    // Waiting queue for tasks with unmet resource requirements
 int waiting_queue_size = 0;
 
+int check_resource_availability(Task task) {
+    if (task.task_type == 'X' && resources_R1 >= 1 && resources_R2 >= 1)
+        return 1;
+    else if (task.task_type == 'Y' && resources_R2 >= 1 && resources_R3 >= 1)
+        return 1;
+    else if (task.task_type == 'Z' && resources_R1 >= 1 && resources_R3 >= 1)
+        return 1;
+    else
+        return 0;
+}
 void allocate_resources(Task task) {
     if (task.task_type == 'X') {
         resources_R1 -= 1;
